557ebcf1a446b4d8f403d11e0de8d6c553c8638a
bihellzin <oliva.gabriel@outlook.com>
2023-08-23 12:15:52 -0300

    changes geocodes

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -25,15 +25,15 @@ const createClusterCustomIcon = function (cluster) {
 // markers
 const markers = [
   {
-    geocode: [-8.0500767, -34.92497],
-    popUp: "Hello, I am pop up 1",
-  },
-  {
-    geocode: [-8.0500767, -35.02497],
+    geocode: [-8.1000767, -34.89097],
     popUp: "Hello, I am pop up 2",
   },
   {
-    geocode: [-8.0500767, -35.12497],
+    geocode: [-8.1600767, -34.91397],
+    popUp: "Hello, I am pop up 1",
+  },
+  {
+    geocode: [-8.200767, -34.91797],
     popUp: "Hello, I am pop up 3",
   },
 ];
@@ -41,20 +41,19 @@ const markers = [
 export default function MapPage() {
   console.log("loading");
   return (
-    <div style={{ width: "100vw", height: "300px" }}>
-      <MapContainer center={[-8.0500767, -34.92497]} zoom={10}>
-        {/* OPEN STREEN MAPS TILES */}
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {/* WATERCOLOR CUSTOM TILES */}
-        {/* <TileLayer
+    <MapContainer center={[-8.0500767, -34.92497]} zoom={10}>
+      {/* OPEN STREEN MAPS TILES */}
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* WATERCOLOR CUSTOM TILES */}
+      {/* <TileLayer
         attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
       /> */}
-        {/* GOOGLE MAPS TILES */}
-        {/* <TileLayer
+      {/* GOOGLE MAPS TILES */}
+      {/* <TileLayer
         attribution="Google Maps"
         // url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}" // regular
         // url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}" // satellite
@@ -63,23 +62,23 @@ export default function MapPage() {
         subdomains={["mt0", "mt1", "mt2", "mt3"]}
       /> */}
 
-        <MarkerClusterGroup
-          chunkedLoading
-          iconCreateFunction={createClusterCustomIcon}
-        >
-          {/* Mapping through the markers */}
-          {markers.map((marker) => (
-            <Marker
-              key={marker.geocode}
-              position={marker.geocode}
-              icon={customIcon}
-            >
-              <Popup>{marker.popUp}</Popup>
-            </Marker>
-          ))}
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createClusterCustomIcon}
+      >
+        {/* Mapping through the markers */}
+        {markers.map((marker) => (
+          <Marker
+            key={marker.geocode}
+            position={marker.geocode}
+            icon={customIcon}
+          >
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
 
-          {/* Hard coded markers */}
-          {/* <Marker position={[51.505, -0.09]} icon={customIcon}>
+        {/* Hard coded markers */}
+        {/* <Marker position={[51.505, -0.09]} icon={customIcon}>
           <Popup>This is popup 1</Popup>
         </Marker>
         <Marker position={[51.504, -0.1]} icon={customIcon}>
@@ -89,8 +88,7 @@ export default function MapPage() {
           <Popup>This is popup 3</Popup>
         </Marker>
        */}
-        </MarkerClusterGroup>
-      </MapContainer>
-    </div>
+      </MarkerClusterGroup>
+    </MapContainer>
   );
 }
