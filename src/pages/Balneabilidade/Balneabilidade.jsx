06dee9c862073eeb460da85ac44f7fbe291d9a1e
Jucosta123 <146770028+Jucosta123@users.noreply.github.com>
2023-10-14 15:37:30 -0300

    Update Balneabilidade.jsx

--- src/pages/Balneabilidade/Balneabilidade.jsx
+++ src/pages/Balneabilidade/Balneabilidade.jsx
@@ -19,7 +19,16 @@ const Balneabilidade = () => {
             Balneabilidade é o conjunto de fatores que define a qualidade da
             água para fins de recreação de contato primário, ou, seja, contato
             direto e prolongado, onde existe a possibilidade de ingerir
-            quantidades consideráveis de água.
+            quantidades consideráveis de água. 
+            A Balneabilidade se torna impropría porque a água das praias vem 
+            diretamente dos rios e canais, estes que podem carregar altos níveis 
+            de poluição, como o esgoto doméstico e industrial não tratados ou porque 
+            está em época de muitas chuvas, logo o valor da balneabilidade não é algo
+            eterno, se uma praia está impropría hoje, semana que vem ela pode estar propría.
+             
+
+
+            
           </p>
         </Card>
       </div>
