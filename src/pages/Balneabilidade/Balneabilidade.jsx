a782d2bfc5466b5ea9cee3b9a479797a096d2d0d
Jucosta123 <146770028+Jucosta123@users.noreply.github.com>
2023-10-21 18:53:59 -0300

    Update Balneabilidade.jsx

--- src/pages/Balneabilidade/Balneabilidade.jsx
+++ src/pages/Balneabilidade/Balneabilidade.jsx
@@ -20,11 +20,7 @@ const Balneabilidade = () => {
             água para fins de recreação de contato primário, ou, seja, contato
             direto e prolongado, onde existe a possibilidade de ingerir
             quantidades consideráveis de água. 
-            A Balneabilidade se torna impropría porque a água das praias vem 
-            diretamente dos rios e canais, estes que podem carregar altos níveis 
-            de poluição, como o esgoto doméstico e industrial não tratados ou porque 
-            está em época de muitas chuvas, logo o valor da balneabilidade não é algo
-            eterno, se uma praia está impropría hoje, semana que vem ela pode estar propría.
+          
              
 
 
